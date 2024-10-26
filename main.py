dc8384f8cc09cdeef6e0dd224ee378184e17a117
Fatim-coding <timabadjie12@gmail.com>
2024-10-26 15:36:56 +0000

    second commit

---
+++ main.py
@@ -0,0 +1,8 @@
+i = int(input("enter a number"))
+if (i< 16):
+     print ("i is smaller than 16")
+     print ("i'm in if block")
+else:
+     print ("i is greater than 16")
+     print ("i'm in else block")
+print ("i'm not in if and not in else block")
